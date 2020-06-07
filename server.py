a00c58ac49e1a07d46c81c17477ef247bb9fd1e9
peelstnac <chfrn2002@gmail.com>
2020-06-07 01:29:26 -0400

    uncommented stuff

--- server.py
+++ server.py
@@ -11,26 +11,31 @@ import pickle
 # server imports
 from bottle import get, post, run, request, static_file
 
-# data = pd.read_csv('books.csv', encoding="ISO-8859-1")
-# N = 16559  # number of books
+data = pd.read_csv('books.csv', encoding="ISO-8859-1")
+N = 16559  # number of books
 
 
-# def lcs(a, b):
-#     l = max(len(a), len(b))+1
-#     dp = [[0]*l for i in range(l)]
-#     for i in range(len(a)+1):
-#         for j in range(len(b)+1):
-#             if((not i) or (not j)):
-#                 continue
-#             if(a[i-1] == b[j-1]):
-#                 dp[i][j] = dp[i-1][j-1]+1
-#             else:
-#                 dp[i][j] = max(dp[i][j-1], dp[i-1][j])
-#     return dp[len(a)][len(b)]
+def lcs(a, b):
+    l = max(len(a), len(b))+1
+    dp = [[0]*l for i in range(l)]
+    for i in range(len(a)+1):
+        for j in range(len(b)+1):
+            if((not i) or (not j)):
+                continue
+            if(a[i-1] == b[j-1]):
+                dp[i][j] = dp[i-1][j-1]+1
+            else:
+                dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+    return dp[len(a)][len(b)]
 
 
 # # collection of book numbers where the summary is too short for tf_idf
-# black_list = [False]*N
+black_list = [False]*N
+
+for i in range(N):
+    temp_list = re.split(r'\W+', data["desc"][i].lower())
+    if(len(temp_list) < 300):
+        black_list[i] = True
 
 # # precompute term frequencies to lower query time
 # # note that the precompute time is like 4 min
@@ -123,6 +128,7 @@ def find_relevant(terms):
         print(weight[i])
     """
 # server code
+
 @post('/find')
 def find():
     terms = request.body.read().decode('utf-8').split()
@@ -135,3 +141,9 @@ def index():
 
 
 run(port=int(os.environ['PORT']), host='0.0.0.0')
+
+"""
+while True:
+    x = input("Input: ").split(" ")
+    print(find_relevant(x))
+"""
